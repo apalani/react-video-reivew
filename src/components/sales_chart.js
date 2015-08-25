c2bb382d75b0ddd229e1364cb3b6999f442a0c16
Eric Masiello <masielle@advisory.com>
2015-08-25 09:17:26 -0400

    Sales chart/D3 stuff

--- src/components/sales_chart.js
+++ src/components/sales_chart.js
@@ -2,11 +2,14 @@ import React from 'react';
 import d3 from 'd3';
 import {max} from 'underscore';
 import Immutable from 'immutable';
+import SalesChartXAxis from './sales_chart_x_axis';
+import SalesChartYAxis from './sales_chart_y_axis';
 
 const CHART_WIDTH = 1100;
 const CHART_HEIGHT = 250;
 const CHART_PADDING = 20;
 const LEFT_MARGIN = 45;
+const BOTTOM_MARGIN = 20;
 
 class SalesChart extends React.Component {
   render(){
@@ -14,8 +17,6 @@ class SalesChart extends React.Component {
     const numDataPoints = salesStats.length;
 
     if(!numDataPoints) return null;
-
-    debugger;
 
     const maxSales = max(salesStats) * 1.10;
 
@@ -28,7 +29,7 @@ class SalesChart extends React.Component {
       .range([CHART_HEIGHT - CHART_PADDING, 0 + CHART_PADDING]);
 
     const line = d3.svg.line()
-      .x((d,i) => xScale(i) + LEFT_MARGIN)
+      .x((d,i) => xScale(i) + LEFT_MARGIN + 10)
       .y((d) => yScale(d))
       .interpolate('linear');
 
@@ -39,6 +40,19 @@ class SalesChart extends React.Component {
         <div className='chart-wrapper' style={boxStyles}>
           <svg className='cahrt' style={boxStyles}>
             <path className='line' d={line(salesStats)}/>
+            <SalesChartXAxis
+              xScale={xScale}
+              numDataPoints={numDataPoints}
+              width={CHART_WIDTH}
+              height={CHART_HEIGHT}
+              leftMargin={LEFT_MARGIN}
+              bottomMargin={BOTTOM_MARGIN} />
+            <SalesChartYAxis
+              yScale={yScale}
+              maxSales={maxSales}
+              height={CHART_HEIGHT}
+              leftMargin={LEFT_MARGIN}
+              bottomMargin={BOTTOM_MARGIN} />
           </svg>
         </div>
       </div>
